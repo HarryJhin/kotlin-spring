158ada611c1eeda735bc93738adab5282a76b118
주진현 <joojinhyun00@gmail.com>
2024-11-17 15:35:51 +0900

    멀티 모듈로 변경

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,26 @@
+pluginManagement {
+    includeBuild("build-logic")
+    repositories {
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+    }
+}
+
 rootProject.name = "kotlin-spring"
+include("app")
+includeModules("data")
+includeModules("domain")
+includeModules("model")
+
+fun includeModules(prefix: String) {
+    File(rootDir, prefix).listFiles()
+        ?.filter { file: File -> file.isDirectory && File(file, "build.gradle.kts").exists() }
+        ?.map { file: File -> ":$prefix:${file.name}" }
+        ?.toTypedArray()
+        ?.run { include(*this) }
+}

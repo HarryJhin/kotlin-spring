5462a1136d422c63441aa402ac1ede109eaac5da
주진현 <joojinhyun00@gmail.com>
2024-11-17 20:30:16 +0900

    test 플러그인 상세 구성

--- build-logic/convention/src/main/kotlin/KotlinSpringTestPlugin.kt
+++ build-logic/convention/src/main/kotlin/KotlinSpringTestPlugin.kt
@@ -5,10 +5,20 @@ import org.gradle.kotlin.dsl.dependencies
 class KotlinSpringTestPlugin : Plugin<Project> {
     override fun apply(target: Project) {
         with(target) {
+            plugins(
+                "org.jetbrains.kotlin.jvm",
+                "org.jetbrains.kotlin.plugin.spring",
+                "org.springframework.boot",
+                "io.spring.dependency-management",
+            )
+
+            configureKotlinJvm()
             configureTestTask()
 
             dependencies {
-                add("testImplementation", libs.findLibrary("spring-boot-starter-test").get())
+                add("testRuntimeOnly", libs.findLibrary("junit.platform.launcher").get())
+                add("testImplementation", libs.findLibrary("kotlin.test.junit5").get())
+                add("testImplementation", libs.findLibrary("spring.boot.starter.test").get())
             }
         }
     }

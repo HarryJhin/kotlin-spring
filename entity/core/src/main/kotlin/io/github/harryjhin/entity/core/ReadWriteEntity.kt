d60c2622937613e88cabfc5a97f557b90f03daac
주진현 <joojinhyun00@gmail.com>
2024-11-20 21:39:45 +0900

    RO의 자식이 RW인 것은 어색하므로 Read/ReadWrite로 변경

--- entity/core/src/main/kotlin/io/github/harryjhin/entity/core/ReadWriteEntity.kt
+++ entity/core/src/main/kotlin/io/github/harryjhin/entity/core/ReadWriteEntity.kt
@@ -3,20 +3,20 @@ package io.github.harryjhin.entity.core
 import jakarta.persistence.Column
 import jakarta.persistence.EntityListeners
 import jakarta.persistence.MappedSuperclass
-import org.springframework.data.annotation.CreatedBy
-import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.LastModifiedBy
+import org.springframework.data.annotation.LastModifiedDate
 import org.springframework.data.jpa.domain.support.AuditingEntityListener
 import java.time.LocalDateTime
 
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener::class)
-abstract class ROEntity {
+abstract class ReadWriteEntity : ReadEntity() {
 
-    @[Column(name = "created_at") CreatedDate]
-    var createdAt: LocalDateTime? = null
+    @[Column(name = "updated_at") LastModifiedDate]
+    var updatedAt: LocalDateTime? = null
         protected set
 
-    @[Column(name = "created_by") CreatedBy]
-    var createdBy: Long? = null
+    @[Column(name = "updated_by") LastModifiedBy]
+    var updatedBy: Long? = null
         protected set
 }

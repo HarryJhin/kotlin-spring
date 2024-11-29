2bda11d101a0615f75d96638e4a01e1b881a0591
주진현 <joojinhyun00@gmail.com>
2024-11-29 18:32:35 +0900

    refactor: NonNull을 위한 _updatedAt 분리

--- entity/core/src/main/kotlin/io/github/harryjhin/entity/core/ModifiableBaseEntity.kt
+++ entity/core/src/main/kotlin/io/github/harryjhin/entity/core/ModifiableBaseEntity.kt
@@ -2,8 +2,12 @@ package io.github.harryjhin.entity.core
 
 import jakarta.persistence.Column
 import jakarta.persistence.EntityListeners
+import jakarta.persistence.EntityNotFoundException
 import jakarta.persistence.MappedSuperclass
+import jakarta.persistence.Transient
 import org.hibernate.annotations.Comment
+import org.hibernate.annotations.JdbcTypeCode
+import org.hibernate.type.SqlTypes
 import org.springframework.data.annotation.LastModifiedDate
 import org.springframework.data.jpa.domain.support.AuditingEntityListener
 import java.time.LocalDateTime
@@ -14,7 +18,12 @@ abstract class ModifiableBaseEntity<T> : CreatableBaseEntity<T>() {
 
     @LastModifiedDate
     @Comment("수정일시")
-    @Column(name = "updated_at")
-    var updatedAt: LocalDateTime? = null
-        protected set
+    @Column(name = "UPDATED_AT")
+    @Suppress("PropertyName")
+    @JdbcTypeCode(SqlTypes.LOCAL_DATE_TIME)
+    protected var _updatedAt: LocalDateTime? = null
+
+    @get:Transient
+    val updatedAt: LocalDateTime
+        get() = _updatedAt ?: throw EntityNotFoundException("아직 영속되지 않은 엔티티입니다.")
 }

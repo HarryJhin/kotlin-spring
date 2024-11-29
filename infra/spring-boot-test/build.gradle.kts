7097fbf21f7149bd5b461eed5cb8afb2a0811bc5
주진현 <joojinhyun00@gmail.com>
2024-11-29 18:08:29 +0900

    refactor: spring-data-jpa 의존성 추가 및 DB Test 분리

--- infra/spring-boot-test/build.gradle.kts
+++ infra/spring-boot-test/build.gradle.kts
@@ -2,8 +2,9 @@ plugins {
     alias(libs.plugins.kotlin.spring.library)
 }
 
-group = "io.github.harryjhin.data.spring-boot-test"
+group = "io.github.harryjhin.infra.spring-boot-test"
 
 dependencies {
+    implementation(projects.infra.databaseTest)
     implementation(libs.spring.boot.autoconfigure)
 }

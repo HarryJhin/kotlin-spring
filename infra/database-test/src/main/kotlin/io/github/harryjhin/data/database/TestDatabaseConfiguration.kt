3e45944bf50105f65d929d31155cf1f10bc7117c
주진현 <joojinhyun00@gmail.com>
2024-12-01 22:05:32 +0900

    refactor: 불필요한 애노테이션 삭제

--- infra/database-test/src/main/kotlin/io/github/harryjhin/data/database/TestDatabaseConfiguration.kt
+++ infra/database-test/src/main/kotlin/io/github/harryjhin/data/database/TestDatabaseConfiguration.kt
@@ -1,8 +1,6 @@
 package io.github.harryjhin.data.database
 
 import org.springframework.context.annotation.Configuration
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
-@EnableJpaAuditing
 @Configuration(proxyBeanMethods = false)
 class TestDatabaseConfiguration

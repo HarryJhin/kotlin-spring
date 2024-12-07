d83214f7b8738ae59323bd091961cec83687a0ea
주진현 <joojinhyun00@gmail.com>
2024-12-07 21:19:20 +0900

    chore: 트랜잭션 라이브러리 추가

--- app/api/build.gradle.kts
+++ app/api/build.gradle.kts
@@ -1,15 +1,16 @@
 plugins {
     alias(libs.plugins.kotlin.spring.app)
+    alias(libs.plugins.kotlin.spring.test)
 }
 
 group = "io.github.harryjhin.app.api"
 version = "0.0.1-SNAPSHOT"
 
 dependencies {
+    implementation(projects.domain.member)
+
+    implementation(libs.spring.boot.starter.logging)
     implementation(libs.spring.boot.starter.web)
+    implementation(libs.spring.tx)
     implementation(libs.jackson.module.kotlin)
-    implementation(libs.kotlin.reflect)
-    testImplementation(libs.spring.boot.starter.test)
-    testImplementation(libs.kotlin.test.junit5)
-    testRuntimeOnly(libs.junit.platform.launcher)
 }

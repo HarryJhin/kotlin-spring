91724fd4f827ffbb450c50e336a677dbf86ec802
주진현 <joojinhyun00@gmail.com>
2024-12-07 18:28:28 +0900

    refactor: 불필요한 Profile 구분 삭제

--- infra/database/src/main/kotlin/io/github/harryjhin/infra/database/DatabaseConfiguration.kt
+++ infra/database/src/main/kotlin/io/github/harryjhin/infra/database/DatabaseConfiguration.kt
@@ -7,14 +7,10 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.context.annotation.Import
-import org.springframework.context.annotation.ImportResource
 import org.springframework.context.annotation.Primary
-import org.springframework.context.annotation.Profile
 import org.springframework.jdbc.datasource.LazyConnectionDataSourceProxy
 import javax.sql.DataSource
 
-@Profile("!test")
 @Configuration(proxyBeanMethods = false)
 class DatabaseConfiguration {
 

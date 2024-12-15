29b500b5dfd267336e0f922a67f9cf74de8fde9f
주진현 <joojinhyun00@gmail.com>
2024-12-15 12:43:45 +0900

    chore: QueryDSL 별도 구성이 필요한 :entity:core, :domain:core 모듈에 직접 작성

--- domain/core/build.gradle.kts
+++ domain/core/build.gradle.kts
@@ -1,8 +1,29 @@
 plugins {
     alias(libs.plugins.kotlin.spring.library)
+    alias(libs.plugins.kotlin.kapt)
 }
 
 group = "io.github.harryjhin.domain.core"
 
 dependencies {
+    implementation(libs.spring.boot.starter.data.jpa)
+    implementation(libs.querydsl.jpa) {
+        artifact {
+            classifier = "jakarta"
+        }
+    }
+    implementation(libs.querydsl.apt) {
+        artifact {
+            classifier = "jakarta"
+        }
+    }
+    implementation(libs.jakarta.annotation.api)
+    implementation(libs.jakarta.persistence.api)
+
+    kapt(libs.querydsl.apt) {
+        artifact {
+            classifier = "jakarta"
+        }
+    }
+    kapt(libs.spring.boot.configuration.processor)
 }
--- entity/core/build.gradle.kts
+++ entity/core/build.gradle.kts
@@ -1,10 +1,30 @@
 plugins {
     alias(libs.plugins.kotlin.spring.library)
+    alias(libs.plugins.kotlin.kapt)
 }
 
 group = "io.github.harryjhin.entity.core"
 
 dependencies {
-    implementation(projects.infra.database)
     api(libs.spring.boot.starter.data.jpa)
+
+    implementation(libs.querydsl.jpa) {
+        artifact {
+            classifier = "jakarta"
+        }
+    }
+    implementation(libs.querydsl.apt) {
+        artifact {
+            classifier = "jakarta"
+        }
+    }
+    implementation(libs.jakarta.annotation.api)
+    implementation(libs.jakarta.persistence.api)
+
+    kapt(libs.querydsl.apt) {
+        artifact {
+            classifier = "jakarta"
+        }
+    }
+    kapt(libs.spring.boot.configuration.processor)
 }

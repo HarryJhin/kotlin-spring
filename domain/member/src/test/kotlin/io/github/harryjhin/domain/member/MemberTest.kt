f05b8661bb531b31bb9010291ee880198972a3c7
주진현 <joojinhyun00@gmail.com>
2025-01-21 18:28:55 +0900

    test: 변경된 dto에 맞는 테스트로 수정

--- domain/member/src/test/kotlin/io/github/harryjhin/domain/member/MemberTest.kt
+++ domain/member/src/test/kotlin/io/github/harryjhin/domain/member/MemberTest.kt
@@ -3,11 +3,10 @@ package io.github.harryjhin.domain.member
 import io.github.harryjhin.domain.member.port.GetAllMember
 import io.github.harryjhin.domain.member.port.GetMember
 import io.github.harryjhin.domain.member.port.SaveMember
-import io.github.harryjhin.domain.member.projection.CompositeMemberDto
-import io.github.harryjhin.model.email.Email
+import io.github.harryjhin.domain.member.projection.Member
+import io.github.harryjhin.model.email.toEmail
+import io.github.harryjhin.model.member.toRawPassword
 import io.github.harryjhin.model.name.toName
-import io.github.harryjhin.model.member.Username
-import io.github.harryjhin.model.member.RawPassword
 import jakarta.transaction.Transactional
 import kotlin.test.Test
 import kotlin.test.assertEquals
@@ -19,24 +18,19 @@ import org.springframework.boot.test.context.SpringBootTest
 @Transactional
 @SpringBootTest
 class MemberTest @Autowired constructor(
-    private val getMember: GetMember,
     private val saveMember: SaveMember,
+    private val getMember: GetMember,
     private val getAllMember: GetAllMember,
 ) {
 
-    private lateinit var member: CompositeMemberDto
+    private lateinit var member: Member
 
     @BeforeEach
     fun setUp() {
-        val username = Username("tester")
-        val rawPassword = RawPassword("password")
-        val email = Email("tester@gmail.com")
-
         member = saveMember {
             this.name = "테스터".toName()
-            this.email = email
-            this.username = username
-            this.rawPassword = rawPassword
+            this.email = "tester@gmail.com".toEmail()
+            this.rawPassword =  "password".toRawPassword()
         }
     }
 
@@ -56,7 +50,18 @@ class MemberTest @Autowired constructor(
         // given
 
         // when
-        val member = getMember(member.username)
+        val member = getMember(username = member.username)
+
+        // then
+        assertNotNull(member)
+    }
+
+    @Test
+    fun `email 회원 조회`() {
+        // given
+
+        // when
+        val member = getMember(email = member.email)
 
         // then
         assertNotNull(member)

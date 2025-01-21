bc9c43da7793e8c570cc975a7210977b0ea3548d
주진현 <joojinhyun00@gmail.com>
2025-01-22 00:39:40 +0900

    refactor: 수정 가능한 엔티티는 동시에 수정 요청이 생기는 것을 방지하기 위한 version 컬럼 추가

--- entity/core/src/main/kotlin/io/github/harryjhin/entity/core/ModifiableBaseEntity.kt
+++ entity/core/src/main/kotlin/io/github/harryjhin/entity/core/ModifiableBaseEntity.kt
@@ -1,10 +1,15 @@
 package io.github.harryjhin.entity.core
 
-import jakarta.persistence.*
+import jakarta.persistence.Column
+import jakarta.persistence.EntityListeners
+import jakarta.persistence.MappedSuperclass
+import jakarta.persistence.Temporal
+import jakarta.persistence.TemporalType
+import jakarta.persistence.Version
+import java.time.Instant
 import org.hibernate.annotations.Comment
 import org.springframework.data.annotation.LastModifiedDate
 import org.springframework.data.jpa.domain.support.AuditingEntityListener
-import java.time.Instant
 
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener::class)
@@ -18,4 +23,20 @@ abstract class ModifiableBaseEntity(
     @Column(name = "MODIFIED_AT", nullable = false)
     var modifiedAt: Instant = modifiedAt
         protected set
+
+    @Version
+    @Comment("버전")
+    @Column(name = "VERSION", nullable = false)
+    var version: Long = 0
+        protected set
+
+    /**
+     * ### 버전 일치 여부
+     *
+     * 버전이 일치하는지 확인합니다.
+     *
+     * @param version 버전
+     * @return 일치 여부
+     */
+    fun matchVersion(version: Long) = this.version == version
 }

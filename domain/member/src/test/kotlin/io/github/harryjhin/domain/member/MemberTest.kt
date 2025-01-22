318f9d50116f523bc316dd7aad7bc7f7a8d6f00e
주진현 <joojinhyun00@gmail.com>
2025-01-22 17:26:08 +0900

    test: 회원 도메인 테스트 추가

--- domain/member/src/test/kotlin/io/github/harryjhin/domain/member/MemberTest.kt
+++ domain/member/src/test/kotlin/io/github/harryjhin/domain/member/MemberTest.kt
@@ -1,12 +1,12 @@
 package io.github.harryjhin.domain.member
 
-import io.github.harryjhin.domain.member.port.GetAllMember
-import io.github.harryjhin.domain.member.port.GetMember
-import io.github.harryjhin.domain.member.port.SaveMember
-import io.github.harryjhin.domain.member.projection.Member
-import io.github.harryjhin.model.email.toEmail
-import io.github.harryjhin.model.member.toRawPassword
-import io.github.harryjhin.model.name.toName
+import io.github.harryjhin.bootstrap.member.GetAllMember
+import io.github.harryjhin.bootstrap.member.GetMember
+import io.github.harryjhin.bootstrap.member.SaveMember
+import io.github.harryjhin.common.email.toEmail
+import io.github.harryjhin.common.member.Member
+import io.github.harryjhin.common.member.toRawPassword
+import io.github.harryjhin.common.name.toName
 import jakarta.transaction.Transactional
 import kotlin.test.Test
 import kotlin.test.assertEquals
@@ -14,6 +14,9 @@ import kotlin.test.assertNotNull
 import org.junit.jupiter.api.BeforeEach
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.PageRequest
+import org.springframework.data.domain.Sort
 
 @Transactional
 @SpringBootTest
@@ -23,45 +26,68 @@ class MemberTest @Autowired constructor(
     private val getAllMember: GetAllMember,
 ) {
 
-    private lateinit var member: Member
+    private val members: MutableList<Member> = mutableListOf()
 
     @BeforeEach
     fun setUp() {
-        member = saveMember {
-            this.name = "테스터".toName()
-            this.email = "tester@gmail.com".toEmail()
-            this.rawPassword =  "password".toRawPassword()
-        }
+        members.add(
+            saveMember {
+                this.name = "테스터".toName()
+                this.email = "tester1@gmail.com".toEmail()
+                this.rawPassword = "password".toRawPassword()
+            }
+        )
+
+        members.add(
+            saveMember {
+                this.name = "테스터".toName()
+                this.email = "tester2@gmail.com".toEmail()
+                this.rawPassword = "password".toRawPassword()
+            }
+        )
+
+        members.add(
+            saveMember {
+                this.name = "테스터".toName()
+                this.email = "tester3@gmail.com".toEmail()
+                this.rawPassword = "password".toRawPassword()
+            }
+        )
+
+        members.add(
+            saveMember {
+                this.name = "테스터".toName()
+                this.email = "tester4@gmail.com".toEmail()
+                this.rawPassword = "password".toRawPassword()
+            }
+        )
+
+        members.add(
+            saveMember {
+                this.name = "테스터".toName()
+                this.email = "tester5@gmail.com".toEmail()
+                this.rawPassword = "password".toRawPassword()
+            }
+        )
     }
 
     @Test
-    fun `member_id 회원 조회`() {
+    fun `회원 조회 #member_id`() {
         // given
 
         // when
-        val member = getMember(member.memberId)
+        val member = getMember(memberId = members.first().memberId)
 
         // then
         assertNotNull(member)
     }
 
     @Test
-    fun `username 회원 조회`() {
+    fun `회원 조회 #username`() {
         // given
 
         // when
-        val member = getMember(username = member.username)
-
-        // then
-        assertNotNull(member)
-    }
-
-    @Test
-    fun `email 회원 조회`() {
-        // given
-
-        // when
-        val member = getMember(email = member.email)
+        val member = getMember(username = members.first().username)
 
         // then
         assertNotNull(member)
@@ -72,12 +98,53 @@ class MemberTest @Autowired constructor(
         // given
 
         // when
-        val entities = getAllMember()
+        val results = getAllMember()
 
         // then
         assertEquals(
-            expected = 1,
-            actual = entities.size,
+            expected = members.size,
+            actual = results.size,
+        )
+    }
+
+    @Test
+    fun `회원 전체 조회 #sort`() {
+        // given
+
+        // when
+        val results = getAllMember(sort = Sort.by(Sort.Order.desc("id")))
+            .onEach(::println)
+
+        // then
+        assertEquals(
+            expected = members.size,
+            actual = results.size,
+        )
+
+        assertEquals(
+            expected = members.last().memberId,
+            actual = results.first().memberId,
+        )
+    }
+
+    @Test
+    fun `회원 전체 조회 #pageable`() {
+        // given
+        val pageable = PageRequest.of(1, 2, Sort.by(Sort.Order.desc("email")))
+
+        // when
+        val page: Page<Member> = getAllMember(pageable = pageable)
+            .onEach(::println)
+
+        // then
+        assertEquals(
+            expected = members.size,
+            actual = page.totalElements.toInt(),
+        )
+
+        assertEquals(
+            expected = pageable.pageSize,
+            actual = page.size,
         )
     }
 }
